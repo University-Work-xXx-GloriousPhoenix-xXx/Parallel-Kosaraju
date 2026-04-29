524416c7e0671d89b655050ab5f56ddcde2eafbc
RomanPR <prikhodjko.roman@gmail.com>
2026-04-29 17:31:07 +0300

    save

--- ParallelKosaraju/ParallelKosaraju/Program.cs
+++ ParallelKosaraju/ParallelKosaraju/Program.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 
 var edgeRatio = 5d;
 
-Console.WriteLine("Enter the edge ratio to vertices:");
+Console.Write("Enter the edge ratio to vertices: ");
 if (double.TryParse(Console.ReadLine(), CultureInfo.InvariantCulture, out var parsedRatio))
 {
     edgeRatio = parsedRatio;
